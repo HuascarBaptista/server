8572853222d6efb9141f5dbd445ac3fd3135d7a2
Huascar Baptista <Huascar.bap@gmail.com>
2012-02-21 04:01:56 -0430

    Server commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerAti::Application.config.secret_token = 'ea91da8b14c6940610e93598185086870e6bf2a6cf8c917662cd20325528487070d08283b309fb9c48d4f2151a555d59d23674abd8bbbf7ca5f8675514b1ebea'
